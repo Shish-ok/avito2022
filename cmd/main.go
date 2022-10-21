fb82b7478e6c6e37ffeeabb5b1b78f0418802225
Shish-ok <alexeyshish92@gmail.com>
2022-10-22 01:57:12 +0300

    make config

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"avito2022/internal/app/config"
+	"context"
+	"github.com/gin-gonic/gin"
+	"go.uber.org/fx"
+)
 
 func main() {
-	fmt.Println("Hello, Avito!")
+	fx.New(
+		fx.Provide(
+			context.Background(),
+			config.NewConfig(),
+			gin.Default(),
+		),
+	).Run()
 }
